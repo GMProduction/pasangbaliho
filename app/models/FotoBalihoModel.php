fd452c967eb7f70cf8c395ebe4887aaec7cbfe7a
pradanna <pradanamahendra@gmail.com>
2019-10-22 15:13:39 +0700

    h

---
+++ app/models/FotoBalihoModel.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Passport\HasApiTokens;
+
+class BalihoModel extends Model
+{
+    use Notifiable;
+    use HasApiTokens, Notifiable;
+    protected $table = "foto_baliho";
+    protected $primary = 'id_foto';
+
+    protected $fillable = [
+        'id_foto','id_baliho','url_foto'
+    ];
+
+}
