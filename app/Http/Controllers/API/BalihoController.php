485641617f2a8057dac377cccd5b2ebc801ee96e
pradanna <pradanamahendra@gmail.com>
2019-10-25 14:38:20 +0700

    b

--- app/Http/Controllers/API/BalihoController.php
+++ app/Http/Controllers/API/BalihoController.php
@@ -41,13 +41,43 @@ class BalihoController extends Controller
         }
     }
 
+    public function dataListAllBalihoByKategori(Request $request)
+    {
+
+        try {
+            $baliho = BalihoModel::join('foto_baliho', 'balihos.id_baliho', 'foto_baliho.id_baliho')
+                ->select(
+                    'balihos.id_baliho as id_baliho',
+                    'balihos.nama_baliho as nama_baliho',
+                    'balihos.alamat as alamat',
+                    'balihos.kategori as kategori',
+                    'balihos.deskripsi as deskripsi',
+                    'foto_baliho.url_foto as url_foto'
+                )
+                ->where('kategori', $request->kategori)
+                ->groupBy('id_baliho')
+                ->get();
+
+            return response()->json([
+                'respon' => 'success',
+                'message' => 'success fetch data baliho',
+                'baliho' => $baliho
+            ]);
+        } catch (\Exception $e) {
+            return response()->json([
+                'respon' => 'failure',
+                'message' => 'terjadi kesalahan ' . $e
+            ], 500);
+        }
+    }
+
     public function showDetailBaliho($id)
     {
         try {
             $baliho = BalihoModel::where('id_baliho', $id)
                 ->first();
 
-                $fotos = FotoBalihoModel::Where('id_baliho', $id)
+            $fotos = FotoBalihoModel::Where('id_baliho', $id)
                 ->get();
 
             return response()->json([
