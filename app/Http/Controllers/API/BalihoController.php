50fbbcbd28989b5f6a5b088a1225c83d9a911759
pradanna <pradanamahendra@gmail.com>
2019-10-28 13:53:19 +0700

    as

--- app/Http/Controllers/API/BalihoController.php
+++ app/Http/Controllers/API/BalihoController.php
@@ -75,6 +75,43 @@ class BalihoController extends Controller
         }
     }
 
+    public function dataListBalihoSearchGlobal(Request $request)
+    {
+        $tambahan = $request->tambahan;
+        try {
+            $baliho = BalihoModel::join('foto_baliho', 'balihos.id_baliho', 'foto_baliho.id_baliho')
+                ->select(
+                    'balihos.id_baliho as id_baliho',
+                    'balihos.nama_baliho as nama_baliho',
+                    'balihos.alamat as alamat',
+                    'balihos.kategori as kategori',
+                    'balihos.deskripsi as deskripsi',
+                    'foto_baliho.url_foto as url_foto'
+                )
+                ->where("kota", $request->kota)
+                ->orwhere("kategori", $request->kategori)
+                ->where(function ($q) use ($tambahan) {
+                    $q->where('ukuran_baliho', 'LIKE', '%' . $tambahan . '%')
+                        ->orwhere('alamat', 'LIKE', '%' . $tambahan . '%')
+                        ->orwhere('provinsi', 'LIKE', '%' . $tambahan . '%');
+                })
+                ->groupBy('id_baliho')
+                ->sortBy($request->sortby)
+                ->get();
+
+            return response()->json([
+                'respon' => 'success',
+                'message' => 'success fetch data baliho',
+                'baliho' => $baliho
+            ]);
+        } catch (\Exception $e) {
+            return response()->json([
+                'respon' => 'failure',
+                'message' => 'terjadi kesalahan ' . $e
+            ], 500);
+        }
+    }
+
     public function showDetailBaliho($id)
     {
         try {
