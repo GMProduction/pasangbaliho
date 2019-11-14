1f520537f6b7cea0154bc15269530242dab790fe
pradanna <pradanamahendra@gmail.com>
2019-11-14 12:36:03 +0700

    asd

--- app/Http/Controllers/FCM/SenderMessage.php
+++ app/Http/Controllers/FCM/SenderMessage.php
@@ -13,13 +13,9 @@ class SenderMessage extends Controller
     {
         fcm()
             ->to(['fc_bIrit9ew:APA91bHYHs-rbHxnL1z-rjAU7EzecZ8ygdpK6rQ82CGefb4xGHFUbWjFTnU_C0SD4FmPYzSL4CU3jcpGxpIUbfVI8TFt7QeA6KYTY1nuzp3SWeONz7kfrxqEFHe4NSxDFBfi5QCVoirG']) // $recipients must an array
-            ->priority('normal')
+            ->priority('high')
             ->timeToLive(0)
             ->data([
-                'title' => 'Test FCM',
-                'body' => 'This is a test of FCM',
-            ])
-            ->notification([
                 'title' => 'Test FCM',
                 'body' => 'This is a test of FCM',
             ])
