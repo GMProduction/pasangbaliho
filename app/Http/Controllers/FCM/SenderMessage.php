0b630de495bab00dc86040f385d4f5c76e0dd1f6
pradanna <pradanamahendra@gmail.com>
2019-11-14 23:30:10 +0700

    gg

--- app/Http/Controllers/FCM/SenderMessage.php
+++ app/Http/Controllers/FCM/SenderMessage.php
@@ -10,15 +10,14 @@ class SenderMessage extends Controller
 
 
     public function sendNotificaton($to = '', $data = array())
-    {
-
-    }
+    { }
 
     public function toFcm()
     {
         $to = "fc_bIrit9ew:APA91bHYHs-rbHxnL1z-rjAU7EzecZ8ygdpK6rQ82CGefb4xGHFUbWjFTnU_C0SD4FmPYzSL4CU3jcpGxpIUbfVI8TFt7QeA6KYTY1nuzp3SWeONz7kfrxqEFHe4NSxDFBfi5QCVoirG";
         $data = array(
-            'body' => 'New Message'
+            'title' => 'Pesan',
+            'body' => 'Pesanan anda sudah dalam proses'
         );
 
         $apikey = 'AAAAAkPEgO0:APA91bHHWjxxeA6d66UHQezAKGc7IiQ-DTt64daEsYx6PLHPuB0cUQSDke3lrQ2GzTojdViVqIViFbJGsJcGiWZVb-Lgj51HWTe91Zq1rP21Taw2SpwrTY9D8M3EWCg-6QGcPYtTk_0B';
@@ -36,7 +35,6 @@ class SenderMessage extends Controller
         curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($fields));
         $result = curl_exec($ch);
         return json_encode($result, true);
-
     }
 
 
