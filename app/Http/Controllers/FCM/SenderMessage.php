4eea0a997e44adeeb25b9ff1056d0b32bc569426
pradanna <pradanamahendra@gmail.com>
2019-11-15 11:22:40 +0700

    asd

--- app/Http/Controllers/FCM/SenderMessage.php
+++ app/Http/Controllers/FCM/SenderMessage.php
@@ -9,7 +9,7 @@ class SenderMessage extends Controller
 {
 
 
-    public function sendNotificaton($to = '', $data = array())
+    public function sendNotificaton()
     { }
 
     public function toFcm()
@@ -18,6 +18,8 @@ class SenderMessage extends Controller
         $data = array(
             'title' => 'Pesan',
             'body' => 'Pesanan anda sudah dalam proses'
+            'icon' => 'https://www.google.com/url?sa=i&source=images&cd=&ved=2ahUKEwiqp-bwruvlAhUKto8KHSLdACwQjRx6BAgBEAQ&url=https%3A%2F%2Fthenounproject.com%2Fterm%2Ftest%2F&psig=AOvVaw0bYw5OCCOgcqEVjQmjMQdJ&ust=1573878079052673'
+            'notification_priority' => 'high'
         );
 
         $apikey = 'AAAAAkPEgO0:APA91bHHWjxxeA6d66UHQezAKGc7IiQ-DTt64daEsYx6PLHPuB0cUQSDke3lrQ2GzTojdViVqIViFbJGsJcGiWZVb-Lgj51HWTe91Zq1rP21Taw2SpwrTY9D8M3EWCg-6QGcPYtTk_0B';
@@ -30,23 +32,11 @@ class SenderMessage extends Controller
         curl_setopt($ch, CURLOPT_POST, true);
         curl_setopt($ch, CURLOPT_HTTPHEADER, $header);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-
         curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
         curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($fields));
         $result = curl_exec($ch);
         return json_encode($result, true);
     }
 
-
-
-    // fcm()
-    //     ->to(['fc_bIrit9ew:APA91bHYHs-rbHxnL1z-rjAU7EzecZ8ygdpK6rQ82CGefb4xGHFUbWjFTnU_C0SD4FmPYzSL4CU3jcpGxpIUbfVI8TFt7QeA6KYTY1nuzp3SWeONz7kfrxqEFHe4NSxDFBfi5QCVoirG']) // $recipients must an array
-    //     ->priority('high')
-    //     ->timeToLive(0)
-    //     ->data([
-    //         'title' => 'Test FCM',
-    //         'body' => 'This is a test of FCM',
-    //     ])
-    //     ->send();
 
 }
