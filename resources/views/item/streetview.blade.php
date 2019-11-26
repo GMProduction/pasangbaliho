b5c7cc3db1b783609443fdd7ccffcf5eecc51dab
pradanna <pradanamahendra@gmail.com>
2019-11-27 00:19:07 +0700

    iframe

--- resources/views/item/streetview.blade.php
+++ resources/views/item/streetview.blade.php
@@ -11,7 +11,7 @@
 @foreach ($street as $s)
 <div class="mapouter">
         <div class="gmap_canvas"><iframe
-                src="{{$s->url_360}}"
+                src={{$s->url_360}}
                 width="100%" height="100%" frameborder="0" style="border:0;" allowfullscreen=""></iframe>
         </div>
 </div>
