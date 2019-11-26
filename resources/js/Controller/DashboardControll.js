bda0274b61367a5b4454a99f5be2601b8aa9dcb9
bagusyanuar <bagus.yanuar613@gmail.com>
2019-11-26 16:14:15 +0700

    asd

--- resources/js/Controller/DashboardControll.js
+++ resources/js/Controller/DashboardControll.js
@@ -1,9 +1,10 @@
 const offurl = 'http://localhost:8000';
+const onurl = 'http://genossys.site'
 
 export async function loadPermintaan () {
     let data = 0;
     try {
-        let res = await axios.get(offurl+'/adminapi/negosiasi/request');
+        let res = await axios.get(onurl+'/adminapi/negosiasi/request');
         data = res.data;
     } catch (error) {
         alert(error)
--- resources/js/Controller/NegosiasiControll.js
+++ resources/js/Controller/NegosiasiControll.js
@@ -1,9 +1,10 @@
 const offurl = 'http://localhost:8000';
+const onurl = 'http://genossys.site'
 
 export async function loadPermintaan () {
     let data = 0;
     try {
-        let res = await axios.get(offurl+'/adminapi/negosiasi/request');
+        let res = await axios.get(onurl+'/adminapi/negosiasi/request');
         data = res.data;
     } catch (error) {
         alert(error)
@@ -14,7 +15,7 @@ export async function loadPermintaan () {
 export async function loadPermintaanHargaById (id) {
     let data = 0;
     try {
-        let res = await axios.get(offurl+'/adminapi/negosiasi/requestById?id='+id);
+        let res = await axios.get(onurl+'/adminapi/negosiasi/requestById?id='+id);
         data = res.data;
     } catch (error) {
         alert(error)
@@ -26,7 +27,7 @@ export async function setHarga (dataUpdate) {
     let data = 0;
 
     try {
-        let res = await axios.post(offurl+'/adminapi/negosiasi/postPrice', dataUpdate);
+        let res = await axios.post(onurl+'/adminapi/negosiasi/postPrice', dataUpdate);
         data = res.data;
     } catch (error) {
         alert(error)
