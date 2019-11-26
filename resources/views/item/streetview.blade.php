4675e5cb9b358847e0ac30ea796bab03d63dda01
pradanna <pradanamahendra@gmail.com>
2019-11-27 00:21:28 +0700

    y

--- resources/views/item/streetview.blade.php
+++ resources/views/item/streetview.blade.php
@@ -12,7 +12,7 @@
 <div class="mapouter">
         <div class="gmap_canvas"><iframe
                 src={{$s->url_360}}
-                width="100%" height="100%" frameborder="0" style="border:0;" allowfullscreen=""></iframe>
+                width="100%" height="100%"></iframe>
         </div>
 </div>
 @endforeach
